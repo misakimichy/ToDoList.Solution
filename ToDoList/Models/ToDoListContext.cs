d5bb57e941967213d290ed44b5e2cde88ddee4b1
Misaki Koonce <misaki.koonce@gmail.com>
2020-01-15 16:23:55 -0800

    Add new table - Category.

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -2,9 +2,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Models
 {
-    // Inherits the Entity Framework class
+  // Inherits the Entity Framework class
   public class ToDoListContext : DbContext
   {
+    public virtual DbSet<Category> Categories { get; set; } 
     public DbSet<Item> Items { get; set; }
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
