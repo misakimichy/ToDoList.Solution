b52200af37f7b97987e5d327ec744ae201c1b68f
Misaki Koonce and Will Quanstrom <misaki@>
2020-01-21 08:42:29 -0800

    Add CategoryItem and migrations.

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -1,4 +1,4 @@
-  using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
@@ -6,11 +6,11 @@ namespace ToDoList.Models
     {
         public Category()
         {
-            this.Items = new HashSet<Item>();
+            this.Items = new HashSet<CategoryItem>();
         }
 
         public int CategoryId { get; set; }
         public string Name { get; set; }
-        public virtual ICollection<Item> Items { get; set; }
+        public virtual ICollection<CategoryItem> Items { get; set; }
     }
 }
--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -2,11 +2,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Models
 {
-  // Inherits the Entity Framework class
   public class ToDoListContext : DbContext
   {
-    public virtual DbSet<Category> Categories { get; set; } 
+    public virtual DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; }
+    public DbSet<CategoryItem> CategoryItem { get; set; }
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
   }
